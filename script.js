709ad47643669b0807be2ae50f4556062539e78f
abdulhadi heyru <101140054+abdiiiiiiiii@users.noreply.github.com>
2024-02-09 23:28:28 +0300

    commit

--- script.js
+++ script.js
@@ -6,5 +6,10 @@ function add() {
     const input = document.getElementById("input").value;
     const li = document.createElement('li');
     li.innerText = input;
+    const button=document.createElement('button')
+    button.innerText="X"
+    button.classList.add("del");
     output.appendChild(li);
+    li.appendChild(button)
 }
+output.addEventListener('click',)
