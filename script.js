4e80f225c44f534e47f829333936749377611dd9
abdulhadi heyru <101140054+abdiiiiiiiii@users.noreply.github.com>
2024-02-11 21:20:58 +0300

    needsedit

--- script.js
+++ script.js
@@ -5,11 +5,40 @@ btn.addEventListener("click", add);
 function add() {
     const input = document.getElementById("input").value;
     const li = document.createElement('li');
-    li.innerText = input;
+    li.textContent = input;
+    output.appendChild(li)  
+    //  appending lists to ul
+
+
+    const checkbox = document.createElement('input');
+    checkbox.type = "checkbox"; 
+    checkbox.classList.add("check") // Setting the type of input to checkbox
+    li.appendChild(checkbox);
+    
+
+
     const button=document.createElement('button')
     button.innerText="X"
     button.classList.add("del");
-    output.appendChild(li);
     li.appendChild(button)
+    
+
+    document.getElementById("input").value = "";
+
 }
-output.addEventListener('click',)
+
+output.addEventListener('click', function(e){
+    if (event.target.classList.contains("del")) {
+        event.target.parentElement.remove();
+    }
+    else if (event.target.classList.contains("check")) {
+        const listItem = event.target.parentElement;
+        if (event.target.checked) {
+            listItem.classList.add("checked");
+        } else {
+            listItem.classList.remove("checked");
+        }
+
+    }
+
+})
